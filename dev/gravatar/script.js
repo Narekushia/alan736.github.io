76113158c16a56298d1059906fb5e17988d9967d
alan736 <alan736officiel@gmail.com>
2014-09-17 21:51:05 +0200

    Gravatar : Update Error Popup
    Still buggy

--- dev/gravatar/script.js
+++ dev/gravatar/script.js
@@ -13,7 +13,16 @@ $(window).resize(CenterMe);
 $(document).ready(function() {
 	setTimeout(CenterMe(),1);
 	$("#TheClic").click(function() {
-		GetGravateImage($('#email').val());
+		var ValideEmail = validateEmail($('#email').val())
+		console.log(ValideEmail);
+
+		if (ValideEmail) {
+			GetGravateImage($('#email').val());
+		}
+		else {
+			var Error = "Ce n'est pas un email valide";
+			NO(Error);
+		}
 	});
 	CenterMe();
 });
@@ -24,3 +33,17 @@ function GetGravateImage(email){
 	$('#image').attr('src', 'http://www.gravatar.com/avatar/' + emailMD5 + "?s=200");
 }
 
+function validateEmail(email) { 
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+} 
+
+function NO(Error) {
+			$('#TheClic')
+  			.popup({
+  				on: 'click',
+          		position : 'bottom center', 
+          		content : Error,
+          		target : '#email'
+  			});
+}

53530b31b3821a7a8d06a188b17beb03a08d8445
Alan736 <alan736officiel@gmail.com>
2014-10-21 14:21:33 +0200

    Youtube API Script : Add alternative version
    Add a alternative way to contact the Youtube API if it's blocked by a proxy.

--- script.js
+++ script.js
@@ -16,16 +16,27 @@ $(document).ready(
             url: "https://gdata.youtube.com/feeds/api/users/alan736",
             dataType: "xml",
             success: function(xml) 
-                     {
+              {
                        $(xml).find('entry').each(   
                          function()
                          {
                             var nb = $(this).find('yt\\:statistics').attr('subscriberCount');
                             $('<span>' + nb + ' abonnés' + '</span>').appendTo('#subscriberCount');
                           });
-                      },
+              },
             error: function() {
-              $('<span>' + "Erreur, Impossible de contacter L'API Youtube" + '</span>').appendTo('#subscriberCount');
+              $.ajax({
+                url : "http://alanovhnku.cluster014.ovh.net/youtubeapi.php",
+                dataType:"jsonp",
+                jsonp:"apicallback",
+                success:function(data)
+                {
+                  $('<span>' + data.subscribers + ' abonnés' + '</span>').appendTo('#subscriberCount');
+                },
+                error: function() {
+                  $('<span>' + "Erreur, Impossible de contacter L'API Youtube" + '</span>').appendTo('#subscriberCount');
+                }
+              });
             }
         });
   }

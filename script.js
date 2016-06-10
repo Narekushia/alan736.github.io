6d2d604918b3fbfe5bf53b0ebc85e994a8040e60
Alan Montavon <alan736officiel@gmail.com>
2016-06-10 02:48:30 +0200

    Change default webpage. "Alan736 : Le Site" has been moved in the "old" folder.

--- script.js
+++ script.js
@@ -1,43 +1,6 @@
-$(document)
-  .ready(function() {
-
-    $('.masthead .information')
-      .transition('scale in')
-    ;
-
-
-  })
-
-$(document).ready(  
- function()
- {
-   $.ajax( {
-            type: "GET",
-            url: "https://gdata.youtube.com/feeds/api/users/alan736",
-            dataType: "xml",
-            success: function(xml) 
-              {
-                       $(xml).find('entry').each(   
-                         function()
-                         {
-                            var nb = $(this).find('yt\\:statistics').attr('subscriberCount');
-                            $('<span>' + nb + ' abonnés' + '</span>').appendTo('#subscriberCount');
-                          });
-              },
-            error: function() {
-              $.ajax({
-                url : "//alanovhnku.cluster014.ovh.net/youtubeapi.php",
-                dataType:"jsonp",
-                jsonp:"apicallback",
-                success:function(data)
-                {
-                  $('<span>' + data.subscribers + ' abonnés' + '</span>').appendTo('#subscriberCount');
-                },
-                error: function() {
-                  $('<span>' + "Erreur, Impossible de contacter L'API Youtube" + '</span>').appendTo('#subscriberCount');
-                }
-              });
-            }
-        });
-  }
-);
+function hover(element) {
+    element.setAttribute('src', 'img/avatar_hidden.jpeg');
+}
+function unhover(element) {
+    element.setAttribute('src', 'img/avatar.png');
+}
